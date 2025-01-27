502e8427d766fa523670cd6e439051975a622808
Brage Sekse Aarset <brage@bjerk.io>
2025-01-27 23:21:02 +0200

    refactor: move client side only code back into index

--- src/pages/map.ts
+++ src/pages/map.ts
@@ -1,9 +1,1 @@
-import maplibregl from "maplibre-gl";
-
-export const map = new maplibregl.Map({
-  container: "map", // container id
-  style:
-    "https://api.maptiler.com/maps/8d052b8a-bcfc-4d5f-8d90-f3af6edc8e13/style.json?key=JzEcmTM9DI9IYqHFODOt", // style URL
-  center: [5.32867, 60.39083], // starting position [lng, lat]
-  zoom: 13.5, // starting zoom
-});
+export const key = "JzEcmTM9DI9IYqHFODOt";
