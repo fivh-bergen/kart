8f0e5cdb0ce3122cdae5b39d857db69e2032858f
Brage Sekse Aarset <brage@bjerk.io>
2025-02-07 16:26:21 +0200

    fix: format "update information" url correctly (#34)
    * fix: format rapid URLs correctly
    
    * docstring
    
    * format

---
+++ src/utils/make-rapid-url.ts
@@ -0,0 +1,10 @@
+/** Makes a direct URL to the rapid editor for a given node  */
+export function makeRapidURL(
+  long: number,
+  lat: number,
+  /** Node id in the format node/918579285 */
+  nodeId: string,
+): string {
+  const rapidId = nodeId.replace("node/", "n");
+  return `https://rapideditor.org/rapid#map=18.34/${long}/${lat}&background=geovekst-nib&id=${rapidId}`;
+}
