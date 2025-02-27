6fe2a6bbe779e6dd5271eea0b2e769969fc93e49
Brage Sekse Aarset <brage@bjerk.io>
2025-02-27 10:44:04 +0200

    feat: set max bounds for map

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,22 @@
+import type { LngLatBoundsLike } from "maplibre-gl";
+
 interface Config {
   startingPosition: {
     lat: number;
     lng: number;
     zoom: number;
   };
+  maxBounds: LngLatBoundsLike;
   style: string;
 }
 
 export const config: Config = {
   style:
     "https://api.maptiler.com/maps/8d052b8a-bcfc-4d5f-8d90-f3af6edc8e13/style.json?key=JzEcmTM9DI9IYqHFODOt",
+  maxBounds: [
+    [4.44, 60.1094],
+    [6.3148, 60.5347],
+  ],
   startingPosition: {
     lng: 5.32867,
     lat: 60.39083, // starting position [lng, lat]
