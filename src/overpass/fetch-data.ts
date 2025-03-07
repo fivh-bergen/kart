5dd5f60d363f3df5a5a6e73e04900f5ad0ebb636
Brage Sekse Aarset <brage@bjerk.io>
2025-03-07 14:07:21 +0200

    refactor: simplify fetch-nodes-script

--- src/overpass/fetch-data.ts
+++ src/overpass/fetch-data.ts
@@ -1,23 +1,26 @@
 import * as fs from "fs/promises";
 import * as path from "path";
+import osmtogeojson from "osmtogeojson";
 
-export async function getFetchUrl(category) {
+export async function getFetchUrl(
+  category: "repair" | "rental" | "second-hand"
+): Promise<string> {
   let filePath;
 
   if (category === "repair") {
     filePath = path.resolve(
       path.dirname(""),
-      "./src/overpass/queries/repair.overpassql",
+      "./src/overpass/queries/repair.overpassql"
     );
   } else if (category === "rental") {
     filePath = path.resolve(
       path.dirname(""),
-      "./src/overpass/queries/rental.overpassql",
+      "./src/overpass/queries/rental.overpassql"
     );
   } else {
     filePath = path.resolve(
       path.dirname(""),
-      "./src/overpass/queries/second-hand.overpassql",
+      "./src/overpass/queries/second-hand.overpassql"
     );
   }
   const data = await fs.readFile(filePath);
@@ -29,16 +32,18 @@ export async function getFetchUrl(category) {
   return url.href + "?" + urlFormatted.toString();
 }
 
-const categories = ["rental", "repair", "second-hand"];
+const categories = ["rental", "repair", "second-hand"] as const;
 
 categories.forEach(async (category) => {
   const url = await getFetchUrl(category);
   const response = await fetch(url);
 
-  const output = await response.text();
+  const output = await response.json();
+
+  const geojson = osmtogeojson(output);
 
   await fs.writeFile(
-    path.resolve(path.dirname(""), `./src/overpass/data/${category}.osmjson`),
-    output,
+    path.resolve(path.dirname(""), `./src/overpass/data/${category}.json`),
+    JSON.stringify(geojson, null, 2)
   );
 });
