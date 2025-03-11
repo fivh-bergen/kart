400e3b075d678149a14f61d21f994baf9ffb9bcf
Brage Sekse Aarset <brage@bjerk.io>
2025-03-11 14:56:11 +0200

    revert config change

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ interface Config {
 
 export const config: Config = {
   style:
-    "https://api.maptiler.com/maps/8d052b8a-bcfc-4d5f-8d90-f3af6edc8e13/style.json?key=lbu0ev46TofwHO0aj2Dw",
+    "https://api.maptiler.com/maps/8d052b8a-bcfc-4d5f-8d90-f3af6edc8e13/style.json?key=JzEcmTM9DI9IYqHFODOt",
   maxBounds: [
     [4.44, 60.1094],
     [6.3148, 60.5347],
