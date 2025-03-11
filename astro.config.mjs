c96afbbeb066333aaf295ad4e4c5d5a5dc6b3caa
Brage Sekse Aarset <brage@bjerk.io>
2025-03-11 11:03:17 +0200

    feat. set up vite PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,44 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fivh-bergen.github.io",
   base: "/kart",
-  integrations: [react()],
+  integrations: [
+    react(),
+    AstroPWA({
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+      manifest: {
+        name: "Gjenbruksportalen",
+        start_url: "/kart",
+        description:
+          "Gjenbruksportalen er til for å gjøre det enklere for deg å finne bruktbutikker, reparasjonssteder og utleiesteder i Bergen.",
+        theme_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/kart/android-chrome-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any maskable",
+          },
+          {
+            src: "/kart/android-chrome-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
+        ],
+        background_color: "#FF7A00",
+      },
+    }),
+  ],
   redirects: {
     "/react": "/kart",
   },
