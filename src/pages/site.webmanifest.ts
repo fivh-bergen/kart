2cf38c8237c1fa605797106ae7915041a24c29f2
Brage Sekse Aarset <brage@bjerk.io>
2025-03-11 10:36:22 +0200

    feat: serve manifest with correct headers in API route (#83)
    * feat: serve manifest with correct header in API route
    
    * format

---
+++ src/pages/site.webmanifest.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = () => {
+  return new Response(
+    JSON.stringify(
+      {
+        name: "Gjenbruksportalen",
+        start_url: "/kart",
+        display: "standalone",
+        background_color: "#FF7A00",
+        theme_color: "#ffffff",
+        description:
+          "Gjenbruksportalen er til for å gjøre det enklere for deg å finne bruktbutikker, reparasjonssteder og utleiesteder i Bergen.",
+        icons: [
+          {
+            src: "/kart/favicon.ico",
+            sizes: "32x32",
+            type: "image/x-icon",
+          },
+        ],
+      },
+      null,
+      2,
+    ),
+    {
+      headers: {
+        "Content-Type": "application/manifest+json",
+      },
+    },
+  );
+};
