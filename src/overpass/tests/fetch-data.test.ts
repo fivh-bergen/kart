e91be5c96f596d5c4511e3fe2e08068af16c11b2
Brage Sekse Aarset <brage@bjerk.io>
2026-01-28 11:53:00 +0100

    remove failing test

--- src/overpass/tests/fetch-data.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { getFetchUrl } from "../fetch-data.js";
-
-describe("getFetchUrl function", () => {
-  it("should URL read overpassQL query correctly", async () => {
-    const actual = await getFetchUrl();
-    expect(actual).toMatchSnapshot();
-  });
-});
