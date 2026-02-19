44f3f8d40a359024f9d8272f298ef95284a4ecb8
Brage Sekse Aarset <brage@bjerk.io>
2026-02-19 16:24:53 +0200

    update docs text

--- src/utils/generate-docs.ts
+++ src/utils/generate-docs.ts
@@ -4,7 +4,7 @@ import * as fs from "fs/promises";
 import { designations } from "./designation";
 
 const table = markdownTable([
-  ["Our tag", "Corresponding OSM tag(s)"],
+  ["Our designation", "Corresponding OSM tag(s)"],
   ...designations.map((designation) => [
     designation.name,
     designation.definitions
@@ -17,16 +17,31 @@ const text = `# Tags
 
 > This file is generated from source code. Do not edit it directly.
 
-  This is a list of tags that are used to provide information about the features in the map. 
-  We don't mean to cover every possible tag in OpenStreetMap since there are [a lot of them](https://wiki.openstreetmap.org/wiki/Tags), but we try tro cover some useful basics. 
+
+In this project, we want to visualise data from OSM in a user friendly way.
+OpenStreetMap data is tagged with key-value pairs, but there are often many different ways to tag the same thing,
+ and the tag data itself is not very pleasing to the eye.
+
+An example would be the tag *clothes=women*, which is not very appealing to read and is not localised.
+Therefore, we want to show this as "Kvinneklær" instead, which is more user friendly.
+We call this a *designation*.
+Since there are often many different ways to tag the same thing in OSM,
+we need to map multiple different OSM tags to the same designation.
+Therefore we have multiple definitions that satisfy a single designation.
+A definition can consist of multiple tags, such as *shop=bicycle* **AND** *repair=yes*,
+which together indicate that this is a place that repairs bicycles.
+
+This is a list of designations that are used in the map. 
+We don't mean to cover every possible tag in OpenStreetMap since there are [a lot of them](https://wiki.openstreetmap.org/wiki/Tags), 
+but we try to cover some useful basics. 
   
-  Some of our tags correspond to multiple different tags in OSM, 
-  since there are often many different ways to tag things in OSM.
+Some of our tags correspond to multiple different tags in OSM, 
+since there are often many different ways to tag things in OSM.
   
 `;
 
 const text2 = `
-## Adding new tags
+## Adding new designations
 Expanding this list is quite straightforward, 
   just add a new tag to the \`tags\` array in [src/utils/tags.ts](../src/utils/tags.ts) and run \`pnpm fetch-nodes\` to update the GeoJSON data and \`pnpm generate\` to update this table.`;
 await fs.writeFile(
